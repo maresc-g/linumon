4ad5b96e0db90702b2be02cea07ff86e80aefc88
Alexis Mestag <mestag_a@epitech.eu>
2014-02-01 17:07:45 +0100

    Fixed UserRepository

--- MMO/Common/include/Database/Repositories/UserRepository.hpp
+++ MMO/Common/include/Database/Repositories/UserRepository.hpp
@@ -5,7 +5,7 @@
 // Login   <mestag_a@epitech.net>
 // 
 // Started on  Sat Feb  1 15:25:50 2014 alexis mestag
-// Last update Sat Feb  1 16:06:47 2014 alexis mestag
+// Last update Sat Feb  1 17:01:57 2014 alexis mestag
 //
 
 #ifndef				__USERREPOSITORY_HPP__
@@ -23,10 +23,7 @@ class				Repository<User> : public Singleton<Repository<User> >
   friend class			Singleton<Repository<User> >;
 
 private:
-  Database::DBType		*_db;
-
-private:
-  Repository() : Singleton(), _db(Database::getInstance()->getDb()) {}
+  Repository() : Singleton() {}
   virtual ~Repository() {}
 
 public:
@@ -35,9 +32,9 @@ public:
       return (u.getPseudo() == pseudo);
     };
     User			*ret = NULL;
-    Database::Transaction	t(_db->begin());
+    Database::Transaction	t(Database::getInstance()->getDb()->begin());
     Database::Query<User>	query(Database::Query<User>::pseudo == pseudo);
-    Database::Result<User>	result(_db->query<User>(query));
+    Database::Result<User>	result(Database::getInstance()->getDb()->query<User>(query));
 
     auto it = std::find_if(result.begin(), result.end(), byPseudoFinder);
 
