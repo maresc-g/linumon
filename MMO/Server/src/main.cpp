d3f0c0b6a2ce53a371f37eb2769379b1f99b81c6
Alexis Mestag <mestag_a@epitech.eu>
2014-02-21 01:34:33 +0100

    (Fix main du serveur [j'avais push mon main de test]). / Le Client ne compile pu depuis que j'ai pull

--- MMO/Server/src/main.cpp
+++ MMO/Server/src/main.cpp
@@ -5,7 +5,7 @@
 // Login   <ansel_l@epitech.net>
 // 
 // Started on  Thu Nov 28 16:45:53 2013 laurent ansel
-// Last update Fri Feb 21 00:48:21 2014 alexis mestag
+// Last update Fri Feb 21 01:31:36 2014 alexis mestag
 //
 
 #include		<string>
@@ -18,63 +18,38 @@
 #include		"Error/SocketError.hpp"
 #include		"CodeBreaker/CodeBreaker.hh"
 
-#include		"Database/Repositories/Repository.hpp"
-
-int			main()
+int			main(int argc, char **argv)
 {
-  Repository<Mob>			*rmm = &Database::getInstance()->getRepository<Mob>();
-  auto					mms = rmm->getAll();
+  Trame			trame;
 
-  std::cout << "Size : " << mms.size() << " (" << mms.empty() << ")" << std::endl;
-  if (!mms.empty()) {
-    for (auto jt = mms.begin() ; jt != mms.end() ; ++jt) {
-      MobModel const			*mm = &(*jt)->getModel();
+  int			ret = 0;
 
-      std::cout << mm->getName() << std::endl;
-      for (auto it = mm->getStats().getStats().cbegin() ; it != mm->getStats().getStats().cend() ; ++it)
-	{
-	  std::cout << "\t" << (*it)->getKey().getName() << " : " << (*it)->getValue() << std::endl;
-	}
+  if (argc == 2)
+    {
+      try
+  	{
+  	  int			port = std::stoi(argv[1]);
+
+  	  srand(time(NULL));
+  	  Server::getInstance()->init(port);
+  	  Server::getInstance()->run();
+  	  Server::deleteInstance();
+  	}
+      catch (std::invalid_argument const &e)
+  	{
+  	  std::cerr << "Invalid port" << std::endl;
+  	  ret = 2;
+  	}
+      catch (SocketError const &e)
+  	{
+  	  std::cerr << e.what() << std::endl;
+  	  ret = 3;
+  	}
     }
-  }
-
-  Repository<MobModel>::deleteInstance();
-  Database::deleteInstance();
-  return (0);
+  else
+    {
+      std::cerr << "Usage:\n\t" << argv[0] << " [port]" << std::endl;
+      ret = 1;
+    }
+  return (ret);
 }
-
-// int			main(int argc, char **argv)
-// {
-//   Trame			trame;
-
-//   int			ret = 0;
-
-//   if (argc == 2)
-//     {
-//       try
-//   	{
-//   	  int			port = std::stoi(argv[1]);
-
-//   	  srand(time(NULL));
-//   	  Server::getInstance()->init(port);
-//   	  Server::getInstance()->run();
-//   	  Server::deleteInstance();
-//   	}
-//       catch (std::invalid_argument const &e)
-//   	{
-//   	  std::cerr << "Invalid port" << std::endl;
-//   	  ret = 2;
-//   	}
-//       catch (SocketError const &e)
-//   	{
-//   	  std::cerr << e.what() << std::endl;
-//   	  ret = 3;
-//   	}
-//     }
-//   else
-//     {
-//       std::cerr << "Usage:\n\t" << argv[0] << " [port]" << std::endl;
-//       ret = 1;
-//     }
-//   return (ret);
-// }
