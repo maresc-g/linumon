fdae8e61fdf1bbf2a8a02565aa2a24dfffc15faa
jourda_c <cyril.jourdain@epitech.eu>
2014-03-02 14:07:23 +0100

    using -j3 to make the client o/ zizi

--- MMO/Client/include/Qt/Views/PlayerClickView.hh
+++ MMO/Client/include/Qt/Views/PlayerClickView.hh
@@ -5,7 +5,7 @@
 // Login   <jourda_c@epitech.net>
 // 
 // Started on  Fri Feb 28 23:00:43 2014 cyril jourdain
-// Last update Sat Mar  1 18:26:47 2014 cyril jourdain
+// Last update Sun Mar  2 14:00:34 2014 cyril jourdain
 //
 
 #ifndef 		__PLAYERCLICKVIEW_HH__
@@ -14,6 +14,8 @@
 #include		<QWidget>
 #include		"Qt/WindowManager.hh"
 #include		"Qt/Views/TradeView.hh"
+
+class TradeView;
 
 class			PlayerClickView : public QWidget
 {
