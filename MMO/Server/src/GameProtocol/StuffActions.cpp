84bfd1d58ca06a251f9759404b95c9c04d3b9a4a
ansel_l <ansel_l@epitech.eu>
2014-03-04 11:26:43 +0100

    move enum eAction, add StyledWrite debug

--- MMO/Server/src/GameProtocol/StuffActions.cpp
+++ MMO/Server/src/GameProtocol/StuffActions.cpp
@@ -5,13 +5,14 @@
 // Login   <ansel_l@epitech.net>
 // 
 // Started on  Sat Feb  8 16:36:22 2014 laurent ansel
-// Last update Sun Feb 16 13:53:35 2014 laurent ansel
+// Last update Tue Mar  4 11:23:40 2014 laurent ansel
 //
 
 #include			<functional>
 #include			<map>
 #include			"GameProtocol/StuffActions.hh"
 #include			"Server/Server.hh"
+#include			"Utility/CommonDefine.hh"
 
 StuffActions::StuffActions()
 {
@@ -28,16 +29,16 @@ StuffActions::~StuffActions()
 bool				StuffActions::stuffAction(Trame *trame)
 {
   bool				ret = false;
-  static std::map<StuffActions::eAction, std::function<bool (Trame *)> > map =
+  static std::map<eStuffAction, std::function<bool (Trame *)> > map =
     {
-      {StuffActions::eAction::GET,	std::bind1st(std::mem_fun(&StuffActions::getStuff), this)},
-      {StuffActions::eAction::PUT,	std::bind1st(std::mem_fun(&StuffActions::putStuff), this)}
+      {eStuffAction::GET,	std::bind1st(std::mem_fun(&StuffActions::getStuff), this)},
+      {eStuffAction::PUT,	std::bind1st(std::mem_fun(&StuffActions::putStuff), this)}
     };
-  eAction			type = NONE;
+  eStuffAction			type = NONE;
 
   if ((*trame)[CONTENT].isMember("STUFF"))
     {
-      type = static_cast<StuffActions::eAction>((*trame)[CONTENT]["STUFF"]["ACTION"].asInt());
+      type = static_cast<eStuffAction>((*trame)[CONTENT]["STUFF"]["ACTION"].asInt());
       ret = map[type](trame);
     }
   return (ret);
