742b17edcec08fba6cc4139a796f88c32ae1b1d3
jourda_c <cyril.jourdain@epitech.eu>
2014-03-24 14:37:55 +0100

    fixed compilation

--- MMO/Client/src/SFML/RessourceSprite.cpp
+++ MMO/Client/src/SFML/RessourceSprite.cpp
@@ -5,7 +5,7 @@
 // Login   <jourda_c@epitech.net>
 // 
 // Started on  Wed Feb 26 14:58:57 2014 cyril jourdain
-// Last update Mon Mar 24 12:41:23 2014 cyril jourdain
+// Last update Mon Mar 24 14:31:59 2014 cyril jourdain
 //
 
 #include		<QMenu>
